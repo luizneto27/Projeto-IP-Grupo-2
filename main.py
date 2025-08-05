407694fe3ca2c042c5d5b6ba8466f293bc3c6f59
luizneto27 <lrsn@cin.ufpe.br>
2025-08-05 15:35:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+# importar as bibliotecas
+import pygame, random
 
+pygame.init() # inicializar o pygame
+clock = pygame.time.Clock() # coloca o jogo pra rodar numa velocidade constante
+
+tela = pygame.display.set_mode((1024, 600)) # dimensao da tela
+cor_tela = (50, 50, 50)
+
+tela.fill(cor_tela)
+
+pygame.display.set_caption('Bem Vindo ao Jogo!')
+running = True
+
+while running: # enquanto running verdeade o jogo roda
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    pygame.display.update()
+
+    humanos = [[], [], []]
+    monstros = []
+    coletaveis = []
+
+    mouse_pos = pygame.mouse.get_pos()
