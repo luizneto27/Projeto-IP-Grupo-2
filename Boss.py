d35d6d4eaccc7ced81b665c4af42137e747aa3f9
Kaua-Wallace <kauamelosilva68@gmail.com>
2025-08-06 14:46:24 -0300

    Create Boss.py

---
+++ Boss.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Boss:
+    def __init__(self, vida_total, pos_x, pos_y, dano):
+        # 1. Armazena a imagem como um atributo da classe (self.image)
+        self.image = pygame.image.load('Imagens/baron_sprite.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (300, 300))
+        
+        # 2. Cria um retângulo (rect) para a imagem e define sua posição
+        self.rect = self.image.get_rect(topleft=(pos_x, pos_y))
+
+        # 3. Armazena os outros atributos corretamente
+        self.vida_total = vida_total
+        self.vida_atual = vida_total # A vida atual começa como total
+        self.dano = dano
+
+    # 4. Cria um método para desenhar o boss na tela
+    def draw(self, tela):
+        tela.blit(self.image, self.rect)
