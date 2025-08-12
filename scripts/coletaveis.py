1dafe918c1d2f803c16f2ac2d6a91771c657933d
luizneto27 <lrsn@cin.ufpe.br>
2025-08-12 19:02:07 -0300

    update

--- scripts/coletaveis.py
+++ scripts/coletaveis.py
@@ -37,12 +37,12 @@ class Moeda(Coletavel):
 class Municao(Coletavel):
     def __init__(self, x, y):
         super().__init__(x, y, 'Imagens/caixa_municao.png', 'municao')
-        self.imagem = pygame.transform.scale(self.imagem, (40, 40))
+        self.imagem = pygame.transform.scale(self.imagem, (50, 50))
 
         # Guarda a posição central original
         old_center = self.rect.center
         
-        self.rect = pygame.Rect(0, 0, 40, 40) 
+        self.rect = pygame.Rect(0, 0, 50, 50) 
         
         # Restaura a posição central
         self.rect.center = old_center
